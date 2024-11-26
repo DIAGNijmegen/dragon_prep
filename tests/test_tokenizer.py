e4d0ef0e84b3ac7fd2d4a7b6db86b5eb951999cf
Joeran Bosma <joeran@bosma.co>
2024-11-26 22:32:00 +0100

    update test

--- tests/test_tokenizer.py
+++ tests/test_tokenizer.py
@@ -2,13 +2,13 @@ from dragon_prep.ner import ner_tokenizer
 
 
 def test_tokenizer():
-    s = 'een "twee"->drie "vier" vijf?->zes zeven?'
+    s = 'een "twee"->drie "vier" vijf?->zes zeven? 8='
     tokenizer = ner_tokenizer()
     tokenized_text = tokenizer(s)
     text_parts = [token.text for token in tokenized_text]
 
     print(text_parts)
-    assert text_parts == ['een', '"', 'twee', '"', '-', '>', 'drie', '"', 'vier', '"', 'vijf', '?', '-', '>', 'zes', 'zeven', '?']
+    assert text_parts == ['een', '"', 'twee', '"', '-', '>', 'drie', '"', 'vier', '"', 'vijf', '?', '-', '>', 'zes', 'zeven', '?', '8=']
 
 
 if __name__ == "__main__":
